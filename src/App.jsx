b6bb281ae20fbd6908ffd9f5884bdc5516ae9271
webhavilah <your-email@example.com>
2025-11-21 18:43:32 +0100

    added header and footer to app

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+    <Header/>
+      <div className='flex space-x-5 justify-center'>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
